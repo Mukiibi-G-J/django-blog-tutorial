aa86d3bcd85b4430c26e88fddb384393e0661c96
unknown <m-g-j@m-g-jose.lubagahospital.org>
2023-01-14 21:27:28 +0300

    Retrieving posts by similarity

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class Post(models.Model):
@@ -22,11 +23,10 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     
+    tags = TaggableManager()
     def get_absolute_url(self):
-        return reverse('blog:post_detail',
-        args=[self.publish.year,
-        self.publish.month,
-        self.publish.day, self.slug])
+     
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
     
     objects = models.Manager()
     published = PostManager()
--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-app_name='blog'
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
